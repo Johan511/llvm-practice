2e5c6ae54d345e472d09c4333c1a59d4ba4ec010
Johan511 <harihara.sn@gmail.com>
2023-02-14 15:26:42 +0530

    ast, cmake. TODO : update how filepath is inputted

---
+++ includes/ast.h
@@ -0,0 +1,65 @@
+#include <string>
+#include <memory>
+#include <vector>
+
+class ExprAST
+{
+public:
+    virtual ~ExprAST() = default;
+};
+
+class NumExprAST : public ExprAST
+{
+    double numVal;
+
+public:
+    NumExprAST(double numVal) : numVal(numVal){};
+};
+
+class VarExprAST : public ExprAST
+{
+    std::string varName;
+
+public:
+    VarExprAST(std::string &varName) : varName(varName){};
+};
+
+class BinExprAST : ExprAST
+{
+    char op;
+    std::unique_ptr<ExprAST> LHS, RHS;
+
+public:
+    BinExprAST(char op, std::unique_ptr<ExprAST> LHS,
+               std::unique_ptr<ExprAST> RHS) : op(op), LHS(std::move(LHS)), RHS(std::move(RHS)) {}
+};
+
+class CallExprAST : public ExprAST
+{
+    std::string callee;
+    std::vector<std::unique_ptr<ExprAST>> args;
+
+public:
+    CallExprAST(std::string &callee,
+                std::vector<std::unique_ptr<ExprAST>> args) : callee(callee), args(std::move(args)) {}
+};
+
+class PrototypeAST
+{
+    std::string prototypeName;
+    std::vector<std::string> argNames;
+
+public:
+    PrototypeAST(std::string &prototypeName, std::vector<std::string> argNames)
+        : prototypeName(prototypeName), argNames(std::move(argNames)){};
+};
+
+class FunctionAST
+{
+    std::unique_ptr<PrototypeAST> prototype;
+    std::unique_ptr<ExprAST> funcBody;
+
+public:
+    FunctionAST(std::unique_ptr<PrototypeAST> prototype,
+                std::unique_ptr<ExprAST> funcBody) : prototype(std::move(prototype)), funcBody(std::move(funcBody)){};
+};
